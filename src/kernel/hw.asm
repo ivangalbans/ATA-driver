d19d52423d40f3ba76e6a7c78e08cf3e8598d923
Hayden Orizondo Marrero <hydn.cu@gmail.com>
2016-10-10 20:40:23 -0400

    Some comments in hw.asm.

--- src/kernel/hw.asm
+++ src/kernel/hw.asm
@@ -1,17 +1,21 @@
 ; These are the routines declared in hw.h.
+[bits 32]
 
 global hw_hlt
 global hw_cli
 global hw_sti
 
+; Invoke hlt.
 hw_hlt:
   hlt
   ret
 
+; Enable interrupts.
 hw_sti:
   sti
   ret
 
+; Disable interrupts.
 hw_cli:
   cli
   ret
